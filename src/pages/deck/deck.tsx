ce71777bec4e34c64393f4c5de31804205bacd39
Lucas Rosa <lucassantosrosa51@gmail.com>
2024-11-19 20:55:13 -0300

    feat(deck): enhance Deck and StreamDeckPlus components with improved layout and interaction

--- src/pages/deck/deck.tsx
+++ src/pages/deck/deck.tsx
@@ -1,32 +1,39 @@
-import {SidebarTrigger} from "@/components/ui/sidebar.tsx";
-import {ResizableHandle, ResizablePanel, ResizablePanelGroup} from "@/components/ui/resizable.tsx";
+import { SidebarTrigger } from "@/components/ui/sidebar.tsx";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable.tsx";
 import DeckDevices from "@/pages/deck/deck-devices.tsx";
 import DeckSettings from "@/pages/deck/deck-settings.tsx";
 import React from "react";
 import StreamDeckPlus from "@/pages/deck/devices/streamdeckplus.tsx";
 
 interface DroppedItem {
-    id: string;
-    name: string;
-    icon: React.ReactNode;
+  id: string;
+  name: string;
+  icon: React.ReactNode;
 }
 
 interface DeckProps {
-    droppedItems?: { [key: string]: DroppedItem[] },
-    selectedDevice?: "streamdeckplus" | "streamdeck"
+  droppedItems?: { [key: string]: DroppedItem[] };
+  selectedDevice?: "streamdeckplus" | "streamdeck";
 }
 
 export default function Deck(props: DeckProps) {
-    return (
-        <div>
-            <ResizablePanelGroup direction="vertical">
-                <SidebarTrigger/>
-                <ResizablePanel defaultSize={0} minSize={0}>
-                    {props.selectedDevice === "streamdeckplus" && <StreamDeckPlus droppedItems={props.droppedItems}/>}
-                </ResizablePanel>
-                <ResizableHandle/>
-                <DeckSettings/>
-            </ResizablePanelGroup>
-        </div>
-    )
+  console.log(props);
+  return (
+    <div>
+      <ResizablePanelGroup direction="vertical">
+        <SidebarTrigger className="bg-gray-800 hover:bg-gray-900 transition-colors" />
+        <ResizablePanel defaultSize={0} minSize={0}>
+          {props.selectedDevice === "streamdeckplus" && (
+            <StreamDeckPlus droppedItems={props.droppedItems} />
+          )}
+        </ResizablePanel>
+        <ResizableHandle />
+        <DeckSettings />
+      </ResizablePanelGroup>
+    </div>
+  );
 }
