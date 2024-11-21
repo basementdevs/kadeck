eeafa12526599fac4c5804c4cb1392cce9c8d6a2
Lucas Rosa <lucassantosrosa51@gmail.com>
2024-11-21 16:05:00 -0300

    feat(stream-deck): add StreamDeckKnobsItem component for displaying dropped items in drop zones

---
+++ src/components/app/stream-deck-knobs-item.tsx
@@ -0,0 +1,41 @@
+import DropZone from "@/pages/deck/deck-droppable-key";
+
+interface DroppedItem {
+  id: string;
+  name: string;
+  icon: React.ReactNode;
+}
+
+interface StreamDeckKnobsItemProps {
+  dropzoneId: string;
+
+  index: number;
+  droppedItems?: {
+    [key: string]: DroppedItem[];
+  };
+}
+
+export function StreamDeckKnobsItem({
+  dropzoneId,
+  index,
+  droppedItems,
+}: StreamDeckKnobsItemProps) {
+  return (
+    <DropZone key={dropzoneId} id={dropzoneId} full>
+      <div
+        key={index}
+        className="bg-gray-700 p-12  rounded-full h-20 w-20 flex items-center justify-center"
+      >
+        {droppedItems?.[dropzoneId]?.map((item) => (
+          <div
+            key={item.id}
+            className="text-white flex items-center gap-2 flex-wrap justify-center"
+          >
+            {item.icon}
+            <span className="">{item.name}</span>
+          </div>
+        ))}
+      </div>
+    </DropZone>
+  );
+}
